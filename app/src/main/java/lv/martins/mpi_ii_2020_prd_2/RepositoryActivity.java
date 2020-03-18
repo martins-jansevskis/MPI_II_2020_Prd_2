04c29a64387e77f5482d71d24a7edc917d473f8a
Martins <martins.jansevskis@gmail.com>
2020-03-18 10:47:24 +0200

    Message

--- app/src/main/java/lv/martins/mpi_ii_2020_prd_2/RepositoryActivity.java
+++ app/src/main/java/lv/martins/mpi_ii_2020_prd_2/RepositoryActivity.java
@@ -2,13 +2,23 @@ package lv.martins.mpi_ii_2020_prd_2;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+
+import org.json.JSONObject;
 
 public class RepositoryActivity extends AppCompatActivity {
 
@@ -19,7 +29,32 @@ public class RepositoryActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//             Hello ,my name is
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://newsapi.org/v2/top-headlines?q=covid-19&?country=lv&from=2020-03-14&sortBy=publishedAt&apiKey=7aded4da9fcd4dee808447686e76b561";
+
+       JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+//                        textView.setText("Response: " + response.toString());
+                    Log.i("Praktiskais", "success:" + response);
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // TODO: Handle error
+                    Log.i("Praktiskais", "error:" + error.toString());
+                }
+            });
+
+        // Access the RequestQueue through your singleton class.
+//                MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
