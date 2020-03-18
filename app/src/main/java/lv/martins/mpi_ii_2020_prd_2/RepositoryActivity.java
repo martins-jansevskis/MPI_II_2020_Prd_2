cb98c1fda44f62ce41dfa05fd1939b58ab95b314
Martins <martins.jansevskis@gmail.com>
2020-03-18 10:19:59 +0200

    Changes

--- app/src/main/java/lv/martins/mpi_ii_2020_prd_2/RepositoryActivity.java
+++ app/src/main/java/lv/martins/mpi_ii_2020_prd_2/RepositoryActivity.java
@@ -19,6 +19,8 @@ public class RepositoryActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
