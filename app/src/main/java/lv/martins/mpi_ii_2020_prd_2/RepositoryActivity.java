3490a526f2f8430ed83e0d40b8711b53ff04dfec
Martins <martins.jansevskis@gmail.com>
2020-03-18 12:22:30 +0200

    Feed

--- app/src/main/java/lv/martins/mpi_ii_2020_prd_2/RepositoryActivity.java
+++ app/src/main/java/lv/martins/mpi_ii_2020_prd_2/RepositoryActivity.java
@@ -1,5 +1,6 @@
 package lv.martins.mpi_ii_2020_prd_2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.android.volley.Request;
@@ -16,10 +17,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -42,33 +51,37 @@ public class RepositoryActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.covid_list);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,  long id) {
+//                Intent intent = new Intent(RepositoryActivity.this, SendMessage.class);
+//                String message = "abc";
+//                intent.putExtra(EXTRA_MESSAGE, message);
+//                startActivity(intent);
+                Toast.makeText(RepositoryActivity.this, "on item click", Toast.LENGTH_LONG).show();
+            }
+        });
 
         // Instantiate the RequestQueue.
-
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://newsapi.org/v2/top-headlines?q=covid-19&?country=lv&from=2020-03-14&sortBy=publishedAt&apiKey=7aded4da9fcd4dee808447686e76b561";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDateandTime = sdf.format(new Date());
+        String url ="https://newsapi.org/v2/top-headlines?q=covid-19&?country=lv&from="+currentDateandTime+"&sortBy=publishedAt&apiKey=7aded4da9fcd4dee808447686e76b561";
 
        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
             (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                 @Override
                 public void onResponse(JSONObject response) {
-                    Log.i("Praktiskais", "success:" + response);
                     try {
                         JSONArray articles = response.getJSONArray("articles");
-                        Log.i("Praktiskais", articles.toString());
-
-//                      izveidot iterāciju cauri articles
                         for (int i = 0; i < articles.length(); i++) {
                             JSONObject article = articles.getJSONObject(i);
-
-                            mylist.add(article.getString("title"));
+                            mylist.add(article.getString("title") + "\n" + article.get("description"));
                         }
                     } catch (Exception e) {
 
                     }
-
-
 
                     adapter.notifyDataSetChanged();
                 }
@@ -92,6 +105,26 @@ public class RepositoryActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.back_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.map:
+                finish();
+//                Intent repoActivity = new Intent(this, MapsActivity.class);
+//                startActivity(repoActivity);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
