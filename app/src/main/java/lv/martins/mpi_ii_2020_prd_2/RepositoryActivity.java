9eba41b9b293f97b447694efe2f36b75fa3dc431
Martins <martins.jansevskis@gmail.com>
2020-03-18 12:58:29 +0200

    Feed reader

--- app/src/main/java/lv/martins/mpi_ii_2020_prd_2/RepositoryActivity.java
+++ app/src/main/java/lv/martins/mpi_ii_2020_prd_2/RepositoryActivity.java
@@ -1,6 +1,8 @@
 package lv.martins.mpi_ii_2020_prd_2;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.android.volley.Request;
@@ -12,17 +14,21 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,7 +43,10 @@ import java.util.ArrayList;
 
 public class RepositoryActivity extends AppCompatActivity {
 
-    ArrayList<String> mylist = new ArrayList<String>();
+    ArrayList<String> titles = new ArrayList<String>();
+    ArrayList<String> descriptions = new ArrayList<String>();
+    ArrayList<String> images = new ArrayList<String>();
+    ArrayList<String> urls = new ArrayList<String>();
 
 
     @Override
@@ -47,18 +56,18 @@ public class RepositoryActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.covid_listview_item, mylist);
+        final MyListAdapter adapter = new MyListAdapter(this, titles, descriptions, images, urls);
 
         ListView listView = (ListView) findViewById(R.id.covid_list);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,  long id) {
-//                Intent intent = new Intent(RepositoryActivity.this, SendMessage.class);
-//                String message = "abc";
-//                intent.putExtra(EXTRA_MESSAGE, message);
-//                startActivity(intent);
-                Toast.makeText(RepositoryActivity.this, "on item click", Toast.LENGTH_LONG).show();
+                String url = urls.get(position);
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                Toast.makeText(RepositoryActivity.this, "on item click: " + urls.get(position), Toast.LENGTH_LONG).show();
             }
         });
 
@@ -77,7 +86,11 @@ public class RepositoryActivity extends AppCompatActivity {
                         JSONArray articles = response.getJSONArray("articles");
                         for (int i = 0; i < articles.length(); i++) {
                             JSONObject article = articles.getJSONObject(i);
-                            mylist.add(article.getString("title") + "\n" + article.get("description"));
+                            titles.add(article.getString("title"));
+                            descriptions.add(article.getString("description"));
+                            urls.add(article.getString("url"));
+                            images.add(article.getString("urlToImage"));
+
                         }
                     } catch (Exception e) {
 
@@ -127,4 +140,43 @@ public class RepositoryActivity extends AppCompatActivity {
         }
     }
 
+    private class MyListAdapter extends ArrayAdapter<String> {
+
+        private final Activity context;
+        private final ArrayList<String> maintitle;
+        private final ArrayList<String> description;
+        private final ArrayList<String> imgid;
+        private final ArrayList<String> url;
+
+        public MyListAdapter(Activity context, ArrayList<String> maintitle, ArrayList<String> description, ArrayList<String> imgid, ArrayList<String> url) {
+            super(context, R.layout.covid_listview_item, maintitle);
+            // TODO Auto-generated constructor stub
+
+            this.context=context;
+            this.maintitle=maintitle;
+            this.description=description;
+            this.imgid=imgid;
+            this.url = url;
+
+        }
+
+        public View getView(int position, View view, ViewGroup parent) {
+            LayoutInflater inflater=context.getLayoutInflater();
+            View rowView=inflater.inflate(R.layout.covid_listview_item, null,true);
+
+            TextView titleText = (TextView) rowView.findViewById(R.id.title);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+            TextView subtitleText = (TextView) rowView.findViewById(R.id.description);
+
+            titleText.setText(maintitle.get(position));
+            Picasso.get().load(images.get(position)).into(imageView);
+//            imageView.setImageResource();
+            subtitleText.setText(description.get(position));
+
+            return rowView;
+
+        };
+    }
+
 }
+
